a878947685337fc7e314b1ac79c9c912b254603d
jieun <dev.wldmsdlsns@gmail.com>
2023-01-02 15:38:07 +0900

    mongoDB Express.js Node.js 연동

---
+++ src/components/Form.js
@@ -0,0 +1,30 @@
+import React, { useCallback } from "react";
+
+const Form = React.memo(({ todoValue, setTodoValue, addTodoSubmit }) => {
+  // console.log("Form Renderind...");
+
+  const changeTodoValue = useCallback((event) => {
+    // 사용자가 입력한 내용 changeTodoValue
+    // console.log(event.target.value);
+    setTodoValue(event.target.value);
+  }, [setTodoValue]) ;
+
+  return (
+    <div>
+      <form onSubmit={addTodoSubmit} className="flex pt-2">
+        <input
+          type="text"
+          placeholder="할 일을 입력하세요."
+          className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
+          value={todoValue}
+          onChange={changeTodoValue}
+        />
+        <input type="submit" 
+        value="등록"
+        className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400" />
+      </form>
+    </div>
+  );
+});
+
+export default Form;
---
+++ src/components/List.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ListItem from "./ListItem";
+
+const List = React.memo( ({todoData, setTodoData, deleteClick}) => {
+  // console.log("List Renderind...");
+    // 함수 컴포넌트에서는 변수나 메서드는 const 나 let을 작성해야해
+
+    
+    
+      
+  return (
+    <div>
+      {todoData.map((item) => (
+        <div key={item.id}>
+          <ListItem  
+          item={item}
+          todoData={todoData} 
+          setTodoData={setTodoData} 
+          deleteClick={deleteClick}
+          />
+      </div>
+      ))}
+    </div>
+  );
+});
+
+export default List;
